24b32849f413d96d6707d4c0b86356639cf14260
Brendan Zotto <brzotto@gmail.com>
2016-09-03 20:58:44 -0400

    new branch test

---
+++ cplus/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Fuck the beautiful world!");
+	return 0;
+}
---
+++ cplus/hello2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Fuck the beautiful world!" << endl;
+	return 0;
+}
